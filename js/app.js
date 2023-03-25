4b387c8fb54443a8d6688e98146bc8030e5a3263
MeliAraoz <melii.araoz96@gmail.com>
2023-03-25 00:59:53 -0300

    Update

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let nombre = prompt("Hola, ingresa tu nombre: ");
+
+//alert("Bienvenido/a " + nombre);
+
+let myDom = document; //
+
+console.log(myDom);
+
+let myApp = document.getElementById("myApp");
+
+myApp.innerHTML = "Bienvenido/a " + nombre; 
